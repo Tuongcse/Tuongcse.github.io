232b87f684686c9483b991298d8941616acfaa42
icewalll <icewall@MacPro.local>
2020-05-18 18:52:37 +0700

    first landding page

---
+++ precache-manifest.996a1b403fa4f8126d0b64b8562856ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37436fb9f20a4df01965d4d6d76bfb08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c076082ae722fae33a",
+    "url": "/static/css/main.34591eaa.chunk.css"
+  },
+  {
+    "revision": "52fcc0b38aa1a359fc94",
+    "url": "/static/js/2.403214df.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/static/js/2.403214df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c076082ae722fae33a",
+    "url": "/static/js/main.8b6b6c99.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
