2df0c717a3473ac4f9a879f743679061fb1a484c
Tuong <nguyenvantuongcsek14@gmail.com>
2020-05-19 13:58:14 +0700

    [update] css

---
+++ precache-manifest.2ffbb9eb9c4ee32e6d8deb26395a9251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0353fe3b7f774f02aedd963b4af0f93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb16045573bc1578968a",
+    "url": "/static/css/main.e0f98007.chunk.css"
+  },
+  {
+    "revision": "52fcc0b38aa1a359fc94",
+    "url": "/static/js/2.403214df.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/static/js/2.403214df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb16045573bc1578968a",
+    "url": "/static/js/main.12ed8b00.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
